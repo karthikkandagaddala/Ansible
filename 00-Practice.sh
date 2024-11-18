47e586fed986d9716b8606cc7ac4bc0aae7ac541
karthikkandagaddala <kandagaddala.karthik888@gmail.com>
2024-11-18 21:38:59 +0530

    practice

---
+++ 00-Practice.sh
@@ -0,0 +1,25 @@
+#! bin/bash/
+userid=$(id -u)
+if [ $userid -ne 0 ]
+then
+     echo "PLease Proceed with Sudo Access"
+     exit 1
+else 
+     echo "Your a super user"
+fi
+validate () {
+    if [ $1 -ne 0 ]
+    then
+         echo "$2 is ....Failure"
+         exit 1
+    else
+         echo "$2 is ....Success"
+         
+    fi
+}
+dnf install mysql -y
+validate ($? "Installing mysql")
+dnf install nginx -y
+validate ($? "Installing nginx")
+dnf remove mysql -y
+validate ($? "Removing mysql")
